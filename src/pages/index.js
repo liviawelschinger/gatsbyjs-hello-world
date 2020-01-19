a0442b472d123951daabaadf1025d118b85ea7c6
liviawelschinger <livia-welschinger@t-online.de>
2020-01-19 09:21:55 +0100

    Link between pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
+import {Link} from "gatsby"
 
 export default () =>
 
     /* JSX */
     <div style={{ color: 'purple'}}>
+    <Link to="/contact/">Contact</Link>
     <h1>Hello Gatsby!</h1>
     <p>What a world </p>
     <img src="https://images.unsplash.com/photo-1565265491739-fc30b45f896a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60"
