d18ba6d508b8c9ff949b03e41143893d2e27fb31
liviawelschinger <livia-welschinger@t-online.de>
2020-01-19 09:00:25 +0100

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () =>
+
+    /* JSX */
+    <div style={{ color: 'purple'}}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world </p>
+    <img src="https://images.unsplash.com/photo-1565265491739-fc30b45f896a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=60"
+        alt=""/>
+    </div>
